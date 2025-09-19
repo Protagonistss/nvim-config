4faae7f2d506a42b640d17ca87f8c543b7a91c3e
huangshan <13837050616@163.com>
2025-09-19 12:48:29 +0800

    feat: 添加Neovim配置文件和lua模块

--- init.lua
+++ init.lua
@@ -1,9 +1,2 @@
 -- Basic Neovim configuration
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-vim.o.termguicolors = true
-
-print("Neovim config loaded successfully!")
+require("core.options")
